b1b529d28fc01e7c17b8481aa6af4ba283fb7a9e
Kaplun <100852322+Tamara-Kaplun@users.noreply.github.com>
2023-11-12 23:49:50 +0300

    Update ttf_testbench.v

--- hw2/ttf_testbench.v
+++ hw2/ttf_testbench.v
@@ -2,23 +2,19 @@
 module testbench; 
 
    reg d;
-	reg clk;
-	reg rstn;
+   reg clk;
+   reg rstn;
+   wire  out;
 
-	wire  out;
-
-	
 	ttf mux(d, clk, rstn, out);
    always #5 clk = ~clk;
 	always #50 rstn = ~rstn;
 	always #30 d = ~d;
     
-    initial begin
-        //$monitor("[%0t] d=0x%0h clk=0x%0h rstn=0x%0h out=0x%0h",$time, d, clk, rstn, out);
-			
-			d = 0;
-			clk = 0; 
-			rstn = 0;
+    initial begin		
+	d = 0;
+	clk = 0; 
+	rstn = 0;
 			       
     end 
 endmodule
